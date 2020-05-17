b2b7a3d397cf4e30255e36e2b880e4b061c6a348
LaunDan <launadaniel2@gmail.com>
2020-05-17 20:35:37 +0200

    B extends V, V extends C, C

--- src/collection/Main.java
+++ src/collection/Main.java
@@ -1,8 +1,20 @@
 package collection;
 
-public class Main {
+public class Main<M extends N, N extends F, F>{
+
+    private M variable;
+
+    public Main(M variable) {
+        this.variable = variable;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+        Main<Integer, Integer, Integer> instance = new Main<>(10);
+
+        System.out.println(instance.compare("10"));
+    }
+
+    public <M2> boolean compare(M2 a){
+        return variable.equals(a);
     }
 }
