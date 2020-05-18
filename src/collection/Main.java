98a9d4ff66e5519954806a695ffcf436049bd80f
LaunDan <launadaniel2@gmail.com>
2020-05-18 16:20:28 +0200

    linkesList

--- src/collection/Main.java
+++ src/collection/Main.java
@@ -3,15 +3,13 @@ package collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-        list.add(5);
-        list.addFirst(6);
-        list.addLast(10);
-        System.out.println(list.getFirst());
-        System.out.println(list.getLast());
+
+        LinkedList list = new LinkedList();
+
     }
 
 }
+
