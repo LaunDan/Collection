82a802b1052c8ca8aa8899400ae2cc65f52cea72
LaunDan <launadaniel2@gmail.com>
2020-05-23 20:07:50 +0200

    created methods pop peek and dispkeyTheStack
    test in psvm

--- src/collection/Main.java
+++ src/collection/Main.java
@@ -9,7 +9,7 @@ public class Main {
     private int stackSize;
     private int topOfStack = -1;
 
-    void TheStack(int size) {
+    public Main(int size) {
         stackSize = size;
         stackArray = new String[size];
         Arrays.fill(stackArray, "-1");
@@ -22,14 +22,55 @@ public class Main {
             stackArray[topOfStack] = input;
         } else System.out.println("Sorry but the stack is full");
         displayTheStack();
-        System.out.println("PUSH " + input + " Was added to the stack");
+        System.out.println("PUSH " + input + " was added to the stack\n");
 
     }
 
-    
+    public String pop() {
+        if (topOfStack >= 0) {
+            displayTheStack();
+            System.out.println("POP " + stackArray[topOfStack] + " was removed from stack\n");
+            stackArray[topOfStack] = "-1";
+            return stackArray[topOfStack--];
+        } else {
+            displayTheStack();
+            System.out.println("Sorry but the stack is empty");
+            return "-1";
+        }
+    }
+
+    public String peek() {
+        displayTheStack();
+        System.out.println("PEEK " + stackArray[topOfStack] + " is at the top of the stack\n");
+        return stackArray[topOfStack];
+
+    }
 
     public static void main(String[] args) {
+        Main theStack = new Main(10);
+        theStack.push("10");
+        theStack.push("4");
+        theStack.peek();
+        theStack.pop();
+    }
 
+    public void displayTheStack() {
+
+        for (int n = 0; n < 61; n++) System.out.print("-");
+        System.out.println();
+        for (int n = 0; n < stackSize; n++) {
+            System.out.format("| %2s " + " ", n);
+        }
+        System.out.println("|");
+        for (int n = 0; n < 61; n++) System.out.print("-");
+        System.out.println();
+        for (int n = 0; n < stackSize; n++) {
+            if (stackArray[n].equals("-1")) System.out.print("|     ");
+            else System.out.print(String.format("| %2s " + " ", stackArray[n]));
+        }
+        System.out.println("|");
+        for (int n = 0; n < 61; n++) System.out.print("-");
+        System.out.println();
     }
 
 }
