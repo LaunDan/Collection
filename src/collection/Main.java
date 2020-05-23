b5aff5b2fb7615130bcc647657848cd30d8aa0c6
LaunDan <launadaniel2@gmail.com>
2020-05-23 18:04:05 +0200

    creating stacks and queues

--- src/collection/Main.java
+++ src/collection/Main.java
@@ -1,79 +1,36 @@
 package collection;
 
 
+import java.util.Arrays;
+
 public class Main {
 
-    private Node first;
-    private Node last;
-    private int size;
+    private String[] stackArray;
+    private int stackSize;
+    private int topOfStack = -1;
 
-    public Main() {
-        size = 0;
+    void TheStack(int size) {
+        stackSize = size;
+        stackArray = new String[size];
+        Arrays.fill(stackArray, "-1");
+
     }
 
-    public int size() {
-        return size;
+    public void push(String input) {
+        if (topOfStack + 1 < stackSize) {
+            topOfStack++;
+            stackArray[topOfStack] = input;
+        } else System.out.println("Sorry but the stack is full");
+        displayTheStack();
+        System.out.println("PUSH " + input + " Was added to the stack");
+
     }
 
-    public boolean isEmpty() {
-        return (size == 0);
+    
+
+    public static void main(String[] args) {
+
     }
 
-    public void add(int number) {
-        Node node = new Node(number);
-        if (isEmpty()) {
-            first = node;
-        } else {
-            last.setNext(node);
-        }
-        last = node;
-        size++;
-    }
-
-    public void remove(int index) {
-        if (index >= size)
-            throw new IndexOutOfBoundsException("Index can't be bigger than " + size);
-        if (index < 0)
-            throw new IllegalArgumentException("Index can't be lower than 0.");
-        if (index == 0) {
-            first = first.getNext();
-        } else {
-            Node node = first;
-            for (int i = 0; i < index - 1; i++) {
-                node = node.getNext();
-            }
-            node.setNext(node.getNext().getNext());
-            if (index == (size - 1)) {
-                last = node;
-            }
-        }
-        size--;
-    }
-
-    public int get(int i) {
-        if (i >= size) {
-            throw new IndexOutOfBoundsException("Out of index:" + i + ", size:" + this.size);
-        }
-        if (i < 0) {
-            throw new IllegalArgumentException("Index lower than 0");
-        }
-        Node node=first;
-        for (int j = 0; j < i; j++) {
-            node = node.getNext();
-        }
-        return node.getValue();
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        Node node=first;
-        for (int i = 0; i < this.size; i++) {
-            builder.append(node.getValue());
-            builder.append(" ");
-            node = node.getNext();
-        }
-        return builder.toString();
-    }
 }
 
