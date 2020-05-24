eb651e1ebf0a13e6a5b10988558f74ba60b5f255
LaunDan <launadaniel2@gmail.com>
2020-05-24 14:22:36 +0200

    created methods priorityInsert, displayTheQueue
    edit test in psvm

--- src/collection/Main.java
+++ src/collection/Main.java
@@ -46,15 +46,58 @@ public class Main {
 
     }
 
+    public void priorityInsert(String input) {
+        int i;
+        if (numberOfItems == 0) {
+            insert(input);
+        } else {
+            for (i = numberOfItems - 1; i >= 0; i--) {
+                if (Integer.parseInt(input) > Integer.parseInt(queueArray[i])) {
+                    queueArray[i + 1] = queueArray[i];
+                }else break;
+            }
+
+            queueArray[i+1]=input;
+            rear++;
+            numberOfItems++;
+        }
+    }
 
     public static void main(String[] args) {
         Main theQueue = new Main(10);
-        theQueue.insert("10");
-        theQueue.insert("11");
-        theQueue.insert("16");
-        theQueue.insert("29");
+        theQueue.priorityInsert("19");
+        theQueue.priorityInsert("11");
+        theQueue.priorityInsert("10");
+        theQueue.priorityInsert("29");
+
+        theQueue.remove();
+
+        theQueue.peek();
 
         theQueue.displayTheQueue();
+
+        theQueue.insert("e4");
+
+        theQueue.displayTheQueue();
+    }
+
+    public void displayTheQueue() {
+
+        for (int n = 0; n < 61; n++) System.out.print("-");
+        System.out.println();
+        for (int n = 0; n < queueSize; n++) {
+            System.out.format("| %2s " + " ", n);
+        }
+        System.out.println("|");
+        for (int n = 0; n < 61; n++) System.out.print("-");
+        System.out.println();
+        for (int n = 0; n < queueSize; n++) {
+            if (queueArray[n].equals("-1")) System.out.print("|     ");
+            else System.out.print(String.format("| %2s " + " ", queueArray[n]));
+        }
+        System.out.println("|");
+        for (int n = 0; n < 61; n++) System.out.print("-");
+        System.out.println();
     }
 
 }
