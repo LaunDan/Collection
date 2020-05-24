a101bfe7b9179af4397bbddcfff1282537190f97
LaunDan <launadaniel2@gmail.com>
2020-05-24 13:48:13 +0200

    del stack and start creating queues

--- src/collection/Main.java
+++ src/collection/Main.java
@@ -1,90 +1,23 @@
 package collection;
 
 
-import java.util.Arrays;
+import java.sql.Array;
 
 public class Main {
 
-    private final String[] stackArray;
-    private final int stackSize;
-    private int topOfStack = -1;
+    private String[] queueArray;
+    private int queueSize;
+    private int front, rear, numberOfItems = 0;
 
-    public Main(int size) {
-        stackSize = size;
-        stackArray = new String[size];
-        Arrays.fill(stackArray, "-1");
-
-    }
-
-    public void push(String input) {
-        if (topOfStack + 1 < stackSize) {
-            topOfStack++;
-            stackArray[topOfStack] = input;
-        } else System.out.println("Sorry but the stack is full");
-        displayTheStack();
-        System.out.println("PUSH " + input + " was added to the stack\n");
-
-    }
-
-    public void pop() {
-        if (topOfStack >= 0) {
-            displayTheStack();
-            System.out.println("POP " + stackArray[topOfStack] + " was removed from stack\n");
-            stackArray[topOfStack] = "-1";
-            topOfStack--;
-        } else {
-            displayTheStack();
-            System.out.println("Sorry but the stack is empty");
-        }
-    }
-
-    public void peek() {
-        displayTheStack();
-        System.out.println("PEEK " + stackArray[topOfStack] + " is at the top of the stack\n");
+    Main(int size) {
+        queueSize = size;
+        queueArray = new String[size];
+        Array.fill(queueArray, "-1");
 
     }
 
     public static void main(String[] args) {
-        Main theStack = new Main(10);
-        theStack.push("10");
-        theStack.push("4");
-        theStack.peek();
-        theStack.pop();
-        theStack.pushMany("12 13 14 15");
-        theStack.popAll();
-        theStack.displayTheStack();
-    }
 
-    public void displayTheStack() {
-
-        for (int n = 0; n < 61; n++) System.out.print("-");
-        System.out.println();
-        for (int n = 0; n < stackSize; n++) {
-            System.out.format("| %2s " + " ", n);
-        }
-        System.out.println("|");
-        for (int n = 0; n < 61; n++) System.out.print("-");
-        System.out.println();
-        for (int n = 0; n < stackSize; n++) {
-            if (stackArray[n].equals("-1")) System.out.print("|     ");
-            else System.out.print(String.format("| %2s " + " ", stackArray[n]));
-        }
-        System.out.println("|");
-        for (int n = 0; n < 61; n++) System.out.print("-");
-        System.out.println();
-    }
-
-    public void pushMany(String multipleValues) {
-        String[] tempString = multipleValues.split(" ");
-        for (String s : tempString) {
-            push(s);
-        }
-    }
-
-    public void popAll() {
-        for (int i = topOfStack; i >= 0; i--) {
-            pop();
-        }
     }
 
 }
