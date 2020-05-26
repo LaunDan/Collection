306fcd207f3dc9b94fd9384693a3c1a029b5ce03
LaunDan <launadaniel2@gmail.com>
2020-05-26 14:33:44 +0200

    rename class Main to Node
    created new class BinaryTree
    
    del old code

---
+++ src/collection/Node.java
@@ -0,0 +1,17 @@
+package collection;
+
+
+import org.w3c.dom.Node;
+
+public class Main {
+
+    int key;
+    Node left, right;
+
+    public Main(int item) {
+        key = item;
+        left = right = null;
+    }
+
+}
+
