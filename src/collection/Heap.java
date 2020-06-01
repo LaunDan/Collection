af24789695d88429021c3a82c39c7d8b04b8ce30
LaunDan <launadaniel2@gmail.com>
2020-06-01 09:37:00 +0200

    done todo

--- src/collection/Heap.java
+++ src/collection/Heap.java
@@ -1,10 +1,6 @@
 package collection;
 
-
-
 public class Heap{
 // TODO make heap and learn how to implement data structures in java
 }
-
-// todo change in git name of files for clone and change foldering
 
